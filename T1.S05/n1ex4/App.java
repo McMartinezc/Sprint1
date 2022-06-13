c643a31db54068bcd844ad00471b5abe8c7dec6b
McMartinezc <105592833+McMartinezc@users.noreply.github.com>
2022-06-13 12:51:22 +0200

    Update App.java

--- T1.S05/n1ex4/App.java
+++ T1.S05/n1ex4/App.java
@@ -1,7 +1,6 @@
 package n1ex4;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -19,9 +18,8 @@ public class App {
 			
 			FileReader llegirFitxer = new FileReader(ruta);
 			BufferedReader br = new BufferedReader(llegirFitxer);
-			String linea = br.readLine();
-						
-			while(linea !=null) {
+								
+			while(linea = br.readLine() !=null) {
 				
 				System.out.println(linea);
 			}
